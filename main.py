e9489cc7ea0fb84d168959b238c63e3de68d461f
OstapFedchuk <ostapit12345@gmail.com>
2023-06-19 09:36:02 +0200

    Alcuni aggiornamenti e lavorazione con il database

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("home.html")
+def home():
+    title = "Home"
+    return render_template("home.html", title=title)
 
 @app.route("/about")
 def about():
